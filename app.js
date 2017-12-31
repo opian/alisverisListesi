f5358b6bd5250d24067ed73300c921a6b237e54b
opian <bekriya@gmail.com>
2017-12-31 14:58:46 +0300

    a

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
-var urunler = document.getElementsByClassName('siparisAdi');
-Array.from(urunler).forEach( function(element, index) {
-	console.log(element.innerHTML);
+//Silme Event Bubling
+
+var liste = document.querySelector('.alisverisListesi');
+liste.addEventListener('click',function(e){
+	if(e.target.className = 'silButon'){
+		liste.removeChild(e.target.parentElement);
+	}
 });
+
+/* silme 
+var silButon = document.querySelectorAll('.silButon');
+Array.from(silButon).forEach( function(element, index) {
+	element.addEventListener('click',function(e){
+		//console.log(e.target.parentElement);
+		e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+	})
+});
+
+*/
+
+//Ekleme
+var ekle = document.querySelector('#ekle');
+//console.log('ekle: ' + ekle);
+var eklenecekUrun = document.querySelector('#urunEkleme');
+var lst = document.querySelector('.alisverisListesi');
+
+ekle.addEventListener('click', function(e){
+	var li  = document.createElement('li');
+	var sAdi = document.createElement('span');
+	sAdi.classList.add('siparisAdi');
+	sAdi.innerHTML = eklenecekUrun.value;
+	var sBtn = document.createElement('span');
+	sBtn.classList.add('silButon');
+	sBtn.innerHTML = 'Sil';
+	li.appendChild(sAdi);
+	li.appendChild(sBtn);
+	lst.appendChild(li);
+	eklenecekUrun.value = '';
+})
+
+
